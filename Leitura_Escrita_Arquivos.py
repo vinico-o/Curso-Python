a6a7897e1d5e33a4dddf2c76979e628d33e72c41
Vinícius Mardegan <vinimardegan2006@gmail.com>
2025-04-14 18:40:57 -0300

    feat: add Leitura_Escrita_Arquivos.py

---
+++ Leitura_Escrita_Arquivos.py
@@ -0,0 +1,17 @@
+#Leitura e Escrita em arquivos
+#Temos alguns modos para abrir o arquivos, leitura (r), escrita (w) e anexar (a)
+
+#leitura de arquivos
+#o arquivo deve ser aberto com open() no modo de leitura, podemos ler o conteudo com read ou readlines
+#read() le o arquivo inteiro, enquanto readlines() somente uma linha
+arquivo = open("teste.txt", "r")
+conteudo = arquivo.read()
+print(conteudo)
+arquivo.close()
+
+#Escrita de arquivos
+#o arquivo deve ser aberto com open() no modo de escrita, se o arquivo nao esxistir, sera criado
+#obs: se o arquivo ja existir, ele sera sobrescrito
+arquivo = open("teste.txt", "w")
+arquivo.write("Vinicius Mardegan")
+arquivo.close()
