96af49e9ce6b9ea6b4242000466cf681fc215b49
Vinícius Mardegan <vinimardegan2006@gmail.com>
2025-04-16 15:58:05 -0300

    feat: add Modulos_Personalizados.py

---
+++ Módulos_Personalizados.py
@@ -0,0 +1,7 @@
+#Para utilizar módulos personalizados, criamos um arquivo .py e definimos o que será utilizado
+
+import modulo_teste
+
+modulo_teste.apresentacao()
+
+modulo_teste.soma(1, 10)
---
+++ modulo_teste.py
@@ -0,0 +1,6 @@
+def apresentacao():
+    print("Olá, eu sou o Vinícius!")
+
+def soma(num1, num2):
+    print(num1 + num2)
+
